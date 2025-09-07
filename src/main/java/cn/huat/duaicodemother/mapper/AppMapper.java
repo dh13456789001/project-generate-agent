9cec9059dc0fc29110dea8d0f4d629a87ccf0097
dh <744854525@qq.com>
2025-09-07 22:50:47 +0800

    实现了以下功能接口： 1.应用管理基础功能，相关的增删改查。 2.ai生成应用代码。 3.应用部署，应用部署至nginx服务器中。 （提交未被管理的代码）

---
+++ src/main/java/cn/huat/duaicodemother/mapper/AppMapper.java
@@ -0,0 +1,13 @@
+package cn.huat.duaicodemother.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import cn.huat.duaicodemother.model.entity.App;
+
+/**
+ * 应用 映射层。
+ *
+ * @author <a href="https://github.com/dh13456789001">duhui</a>
+ */
+public interface AppMapper extends BaseMapper<App> {
+
+}
---
+++ src/main/java/cn/huat/duaicodemother/service/AppService.java
@@ -0,0 +1,57 @@
+package cn.huat.duaicodemother.service;
+
+import cn.huat.duaicodemother.model.dto.app.AppQueryRequest;
+import cn.huat.duaicodemother.model.entity.User;
+import cn.huat.duaicodemother.model.vo.AppVO;
+import com.mybatisflex.core.query.QueryWrapper;
+import com.mybatisflex.core.service.IService;
+import cn.huat.duaicodemother.model.entity.App;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
+/**
+ * 应用 服务层。
+ *
+ * @author <a href="https://github.com/dh13456789001">duhui</a>
+ */
+public interface AppService extends IService<App> {
+
+    /**
+     * 获取应用封装类
+     *
+     * @param app
+     * @return
+     */
+    AppVO getAppVO(App app);
+
+    /**
+     * 获取应用封装类列表
+     *
+     * @param appList
+     * @return
+     */
+    List<AppVO> getAppVOList(List<App> appList);
+
+
+    QueryWrapper getQueryWrapper(AppQueryRequest appQueryRequest);
+
+
+    /**
+     * 聊天生成代码
+     * @param appId 应用ID
+     * @param message 用户提示词
+     * @param loginUser 登录用户
+     * @return AI 生成的代码流
+     */
+    Flux<String> chatToGenCode(Long appId, String message, User loginUser);
+
+
+    /**
+     *
+     * @param appId
+     * @param loginUser
+     * @return
+     */
+    String deployApp(Long appId, User loginUser);
+}
